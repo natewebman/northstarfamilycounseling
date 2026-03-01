3ee50dc61cf12908eca79f4b325de5d0f7617750
natewebman <natecorrieri@gmail.com>
2026-03-01 10:16:39 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://northstarfamilycounseling.com',
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
-  output: 'static',
+    site: 'https://northstarfamilycounseling.com',
+    integrations: [
+          tailwind(),
+        ],
+    output: 'static',
 });
